db02b7b4c4d18007079e1ed50e69c655fd4bb1f8
arredocana <arredocana@gmail.com>
2022-05-13 23:33:31 +0200

    remove wait_on_rate_limit_notify

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ TWITTER_ACCESS_TOKEN_SECRET = os.environ.get('TWITTER_ACCESS_TOKEN_SECRET')
 def create_twitter_api():
     auth = tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_API_SECRET)
     auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
-    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+    api = tweepy.API(auth, wait_on_rate_limit=True)
 
     try:
         api.verify_credentials()
